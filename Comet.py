3d7b31bab396be847f70352e1dfa0e1f93bcba9b
GabrielSolache <solachegj445@mail.sandhills.edu>
2025-09-10 04:51:54 +0000

    comet.py

---
+++ Comet.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a number"))
+b = int(input("Enter second number"))
+c = int(input("Enter third number"))
+result = (a + b) / c
+print(result)
+if result > 0:
+    print ("The mathematical operation is > 0")
+else:
+    print ("The mathematical operation is <= 0")
