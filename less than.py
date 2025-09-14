6b31a3ce5aba91e942395aa87e8fb029a6b044e9
GabrielSolache <solachegj445@mail.sandhills.edu>
2025-09-14 21:48:10 +0000

    no

--- less than.py
+++
@@ -1,8 +0,0 @@
-x = int (input("Enter a number"))
-y = int (input("Enter second number"))
-diff = x - y 
-print(diff)
-if diff < 0:
-    print("Invalid! The value is less than 0")
-else:
-    print("The values entered were valid integers")
